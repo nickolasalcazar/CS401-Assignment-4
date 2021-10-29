a68948f74272e053deeb0deaf0924d025a0176ab
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-10-29 09:42:17 -0700

    Added methods

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -19,20 +19,29 @@ public class ShoppingCart {
 	LocalDate getDateCreated() { return created; }
 
 	/**
-	 * Returns lineItems.
+	 * Returns lineItems, an ArrayList containing the LineItems of
+	 * ShoppingCart.
 	 */
-	public ArrayList<LineItem> getLineItems() {
-		return lineItems;
-	}
+	public ArrayList<LineItem> getLineItems() { return lineItems; }
 
 	/**
-	 * Remove a quantity of a product from the ShoppingCart. Removes
-	 * a Product object from the ArrayList lineItems.
+	 * Remove a quantity of a Product from ShoppingCart. Removes a Product
+	 * object from the ArrayList lineItems.
 	 * 
 	 * @param product	Product to remove.
-	 * @param qty		Quantity of the the product.
+	 * @param price		Price of LineItem.
+	 * @param qty		Quantity of the Product to remove.
 	 */
-	//public void addProduct(Product product, int qty) { }
+	public void addLineItem(Product product, float price, int qty) {
+		for (int i = 0; i < lineItems.size(); i++) {
+			// If product already exists in lineItems, add extra qty
+			if (lineItems.get(i).getProduct().equals(product)) {
+				lineItems.get(i).setQty(lineItems.get(i).getQty() + qty);
+				return;
+			}
+		}
+		lineItems.add(new LineItem(product, price, qty));
+	}
 
 	// public void removeProduct(Product product, int qty) { }
 }
