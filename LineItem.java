441d877248f26d31993cf13cb99e3497d2c9c222
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-10-29 09:13:05 -0700

    Added methods to LineItem.java

--- LineItem.java
+++ LineItem.java
@@ -1,3 +1,20 @@
+/**
+ * LineItem represents a Product that has been added to a ShoppingCart.
+ * It records the price and quantity of the Product, and a reference to the
+ * Product object that it represents.
+ */
 public class LineItem {
-	
+	private float price;
+	private int qty;
+
+	private Product product;
+
+	public Product(float price, int qty) {
+		this.price = price;
+		this.qty = qty;
+	}
+
+	public float getPrice() { return price; }
+
+	public Product getProduct() { return product; }
 }
