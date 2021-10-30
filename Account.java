abc25878d0f6c90a2fd56d5ab71e53d10d50ef17
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-10-29 17:07:48 -0700

    Removed print statement

--- Account.java
+++ Account.java
@@ -27,12 +27,8 @@ public class Account {
 	public Account(String id, String address) {
 		this.id = id;
 		billing_address = address;
-
 		open = LocalDate.now();
-
 		shoppingCart = new ShoppingCart(); // 1 per Account
-
-		System.out.println("Account opened on " + open.toString());
 	}
 
 	/**
